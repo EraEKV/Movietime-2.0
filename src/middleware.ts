9d047a26f5312b7bc7abacd7099a754c95427f0f
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2025-02-21 21:56:32 +0500

    checking for deploy

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,21 +18,21 @@ export default auth((req) => {
     const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
     if(isApiAuthRoute) {
-        return undefined;
+        return;
     }
 
     if(isAuthRoute) {
         if(isLoggedIn) {
             return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
         }
-        return undefined;
+        return;
     }
 
     if(!isLoggedIn && !isPublicRoute) {
         return Response.redirect(new URL("/sign-in", nextUrl));
     }
 
-    return undefined;
+    return;
 });
 
 export const config = {
